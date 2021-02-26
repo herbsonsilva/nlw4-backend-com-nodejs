016fe9bc6a56d0c2ad98feab809bd0f0da4d4a3c
Herbson Silva <herbsonsilvajp@gmail.com>
2021-02-26 01:10:22 -0300

    Add UserController.ts and routes.ts

--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,13 @@
 import 'reflect-metadata';
 import './database';
 import express from 'express';
+import { router } from './routes';
 
 const app = express();
 
 app.use(express.json());
 
-app.get('/users', (request, response) => {
-  return response.json({ message: "GET: Hello World!" });
-});
+app.use(router);
 
 app.listen(3333, () => {
   console.log('🚀 Server started on port 3333!');
