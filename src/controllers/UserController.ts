b1312a8aa958204d6d98c58aa6dea489415f93c0
Herbson Silva <herbsonsilvajp@gmail.com>
2021-02-26 01:48:54 -0300

    Update UserController.ts to create users

--- src/controllers/UserController.ts
+++ src/controllers/UserController.ts
@@ -1,10 +1,31 @@
 import { Request, Response } from 'express';
+import { getRepository } from 'typeorm';
+import { User } from '../models/User';
 
 class UserController {
   async create(request: Request, response: Response) {
-    const body = request.body;
-    console.log(body);
-    response.send();
+    const { name, email } = request.body;
+
+    const usersRepository = getRepository(User);
+
+    const userAlreadExists = await usersRepository.findOne({
+      email
+    });
+
+    if (userAlreadExists) {
+      return response.status(400).json({
+        error: 'User alread exists!'
+      });
+    }
+
+    const user = usersRepository.create({
+      name,
+      email
+    });
+
+    await usersRepository.save(user); 
+    
+    response.json(user);
   };
 };
 
