971d0769e027926758701639bf85601409661597
Herbson Silva <herbsonsilvajp@gmail.com>
2021-02-25 23:19:37 -0300

    Add ormconfig.json and database/index.ts

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,15 @@
+import 'reflect-metadata';
+import './database';
 import express from 'express';
 
 const app = express();
 
+app.use(express.json());
+
+app.get('/users', (request, response) => {
+  return response.json({ message: "GET: Hello World!" });
+});
+
 app.listen(3333, () => {
-  console.log('🚀 Server started on port 3333!')
-})
+  console.log('🚀 Server started on port 3333!');
+});
